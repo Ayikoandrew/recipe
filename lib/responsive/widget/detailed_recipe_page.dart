fd6fb3ba14d3a795ebf01585c398b33c69e8e238
ayiko <andrewayiko40@gmail.com>
2025-07-27 15:18:50 +0300

    feat: create DetailedRecipePage widget to display recipe details

---
+++ lib/responsive/widget/detailed_recipe_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:recipe/model/recipe.dart';
+
+class DetailedRecipePage extends StatelessWidget {
+  const DetailedRecipePage({super.key, required this.recipe});
+  final Recipe recipe;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
